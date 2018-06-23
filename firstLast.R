96d28bac298a81d0e0acd6ba7a7bd18529d80eb9
DavidMargraf <margr005@umn.edu>
2018-06-23 13:14:23 -0500

    basic functionality added to each function

--- firstLast.R
+++ firstLast.R
@@ -1,5 +1,49 @@
-first <- function(df,f){}
+# Functions to find the first, and/or last observations 
+#  based on a particular key variable such as ID number.
+first <- function(x, key){
+  a <- deparse(substitute(x))
+  b <- deparse(substitute(key))
+  nam <- make.names(paste0("first.", b))
+  nam2 <- paste0(a,"$", b)
+  c <- eval(parse(text = nam2))
+  d <- as.numeric(!duplicated(c))
+  assign(nam, d, envir = .GlobalEnv)
+}
 
-last <- function(df,l){}
+last <- function(x, key){
+  a <- deparse(substitute(x))
+  b <- deparse(substitute(key))
+  nam <- make.names(paste0("last.", b))
+  nam2 <- paste0(a,"$", b)
+  c <- eval(parse(text = nam2))
+  d <- as.numeric(!duplicated(c, fromLast = T))
+  assign(nam, d, envir = .GlobalEnv)
+}
 
-firstLast <- function(df,f,l){}
+firstLast <- function(x, key){
+  # first observation
+  a <- deparse(substitute(x))
+  b <- deparse(substitute(key))
+  nam <- make.names(paste0("first.", b))
+  nam2 <- paste0(a,"$", b)
+  c <- eval(parse(text = nam2))
+  d <- as.numeric(!duplicated(c))
+  assign(nam, d, envir = .GlobalEnv)
+  
+  # last observation
+  e <- deparse(substitute(x))
+  f <- deparse(substitute(key))
+  nam3 <- make.names(paste0("last.", f))
+  nam4 <- paste0(a,"$", f)
+  g <- eval(parse(text = nam4))
+  h <- as.numeric(!duplicated(g, fromLast = T))
+  assign(nam3, h, envir = .GlobalEnv)
+}
+
+# Example code
+df <-data.frame(id=rep(seq(10), each=10), thing = rep(c("A","B")))
+df
+
+first(df, id)
+last(df, id)
+firstLast(df, id)
