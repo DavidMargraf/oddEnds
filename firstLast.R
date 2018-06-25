32ece3c0d6b525f41b7d61094b2b1409278995e1
DavidMargraf <margr005@umn.edu>
2018-06-25 11:35:18 -0500

    added messages

--- firstLast.R
+++ firstLast.R
@@ -1,14 +1,42 @@
 # Functions to flag the first, and/or last observations 
 #  based on a particular key variable such as ID number.
-first <- function(x, key){
+first <- function(x, key, verbose = TRUE){
+  
   a <- deparse(substitute(x))
-  b <- deparse(substitute(key))
-  nam <- paste0("first.", b)
-  nam2 <- paste0(a,"$", b)
+  
+  if (missing(key)) {
+    b = names(x[1])
+    # print(key)
+    # print(deparse(substitute(key)))
+    # print(paste0(a, "$", key))
+    nam <- paste0("first.", b)
+    nam2 <- paste0(a, "$", b)
+    # print(nam)
+    # print(nam2)
+    
+    message("Using the first column in that data set since a key variable was not specified. \n")
+
+  } else if (!missing(key)){
+  
+    b <- deparse(substitute(key))
+    nam <- paste0("first.", b)
+    nam2 <- paste0(a,"$", b)
+  }
+  
   c <- eval(parse(text = nam2))
   d <- as.numeric(!duplicated(c))
   assign(nam, d, envir = .GlobalEnv)
-}
+  
+  
+  if (verbose) {
+    message(paste0("The first observations were flagged for the column labeled",
+                   " `", b ,"` ",
+                   "in data set,",
+                   " `", a ,"` ",
+                   "and stored in the vector",
+                   " `", nam ,"`. "))
+  }
+} # end first()
 
 last <- function(x, key){
   a <- deparse(substitute(x))
@@ -45,6 +73,9 @@ df <-data.frame(id=rep(seq(10), each=10), thing = rep(c("A","B")))
 df
 
 library(dplyr)
+df <- data.frame(id=rep(seq(10), each=10)) %>% 
+      mutate(thing = sample(c("A","B"), length(id), replace = T)) 
+
 df <- df %>% mutate(first.id) %>% filter(first.id==1)
 df
 
